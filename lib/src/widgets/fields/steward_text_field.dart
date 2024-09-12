820811694aa4dfaaaffd0f8ae395074c7abe9fb4
Duncan Kariuki <duncankariuki60@gmail.com>
2024-09-12 16:00:25 +0300

    Update StewardTextFieldWidget documentation

--- lib/src/widgets/fields/steward_text_field.dart
+++ lib/src/widgets/fields/steward_text_field.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:form_steward/form_steward.dart';
 
+/// A widget that represents a text field within a form managed by Form Steward.
+///
+/// The [StewardTextFieldWidget] is a customizable text field that works with
+/// the Form Steward state management system to manage field validation and
+/// state updates. This widget listens for validation triggers and updates the
+/// form state accordingly.
+///
+/// The widget receives a [FieldModel] that defines the field's properties such as
+/// label and validation rules. It also receives references to [FormStewardStateNotifier]
+/// to update the form state and [ValidationTriggerNotifier] to trigger field validation.
 class StewardTextFieldWidget extends StatefulWidget {
+  /// The model representing the field's properties and validation rules.
   final FieldModel field;
+
+  /// The name of the step to which this field belongs.
   final String stepName;
+
+  /// The [FormStewardStateNotifier] instance to manage the form state.
   final FormStewardStateNotifier formStewardStateNotifier;
+
+  /// The [ValidationTriggerNotifier] instance used to listen for validation triggers.
   final ValidationTriggerNotifier validationTriggerNotifier;
 
+  /// Creates an instance of [StewardTextFieldWidget].
+  ///
+  /// This widget manages a form field, validates it based on the provided
+  /// [FieldModel], and updates the form state using [FormStewardStateNotifier].
   const StewardTextFieldWidget({
     super.key,
     required this.field,
@@ -20,17 +41,22 @@ class StewardTextFieldWidget extends StatefulWidget {
 }
 
 class _StewardTextFieldWidgetState extends State<StewardTextFieldWidget> {
+  /// Stores the error message to display when validation fails.
   String? _errorMessage;
+
+  /// Stores the current value of the text field.
   String? textValue;
 
   @override
   void initState() {
     super.initState();
+    // Listen for validation trigger events from ValidationTriggerNotifier.
     widget.validationTriggerNotifier.addListener(_onValidationTrigger);
   }
 
   @override
   void dispose() {
+    // Remove the validation trigger listener when the widget is disposed.
     widget.validationTriggerNotifier.removeListener(_onValidationTrigger);
     super.dispose();
   }
@@ -38,17 +64,23 @@ class _StewardTextFieldWidgetState extends State<StewardTextFieldWidget> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      // Value of the TextFormField
+      // Displays the label and error message for the text field.
       decoration: InputDecoration(
         labelText: widget.field.label,
         errorText: _errorMessage,
       ),
       onChanged: (value) {
+        // Update the value of the text field when the user changes the input.
         textValue = value;
       },
     );
   }
 
+  /// Validates the field based on the required validation rule.
+  ///
+  /// If the field is required but no value is provided, an error message is
+  /// displayed, and the form state is updated as invalid. Otherwise, the error
+  /// message is cleared, and the form state is updated as valid.
   void _validate([String? value]) {
     if (widget.field.validation?.required == true &&
         (textValue == null || textValue?.isEmpty == true)) {
@@ -64,6 +96,10 @@ class _StewardTextFieldWidgetState extends State<StewardTextFieldWidget> {
     }
   }
 
+  /// Updates the form state using [FormStewardStateNotifier].
+  ///
+  /// The method updates the form state by passing the field name, the current
+  /// value, and the validity status of the field to [FormStewardStateNotifier].
   void updateState(bool isValid) {
     widget.formStewardStateNotifier.updateField(
         stepName: widget.stepName,
@@ -72,6 +108,9 @@ class _StewardTextFieldWidgetState extends State<StewardTextFieldWidget> {
         isValid: isValid);
   }
 
+  /// Handles the validation trigger when [ValidationTriggerNotifier] triggers validation.
+  ///
+  /// This method listens for validation triggers specific to this field's step.
   void _onValidationTrigger() {
     if (widget.validationTriggerNotifier.value == widget.stepName) {
       _validate(textValue);
