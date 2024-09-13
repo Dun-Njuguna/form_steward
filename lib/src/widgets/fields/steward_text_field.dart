ca1b711fea4483e653338fc0901c2e976f4f2107
Duncan Kariuki <duncankariuki60@gmail.com>
2024-09-13 07:28:56 +0300

    Fix

--- lib/src/widgets/fields/steward_text_field.dart
+++ lib/src/widgets/fields/steward_text_field.dart
@@ -97,6 +97,8 @@ class _StewardTextFieldWidgetState extends State<StewardTextFieldWidget> {
     if (!isValid) {
       updateState(false);
       return;
+    } else {
+      updateState(true);
     }
   }
 
