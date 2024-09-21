febeffebaf23a82431c30d4972e01cec730b3d3f
Duncan Kariuki <duncankariuki60@gmail.com>
2024-09-21 17:55:52 +0300

    Remove old test cases

--- test/form_steward_test.dart
+++ test/form_steward_test.dart
@@ -2,33 +2,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'controllers/form_controller_test.dart';
-import 'controllers/step_controller_test.dart';
-import 'models/field_model_test.dart';
-import 'models/form_step_model_test.dart';
-import 'models/validation_model_test.dart';
-import 'services/api_service_test.dart';
-import 'services/form_service_test.dart';
-import 'utils/json_parser_test.dart';
-import 'utils/validators_test.dart';
-import 'widgets/form_builder_test.dart';
-import 'widgets/form_field_widget_test.dart';
-import 'widgets/stepper_widget_test.dart';
-
 void main() {
-  // Run all tests from the specified files
-  group('All Tests', () {
-    formControllerTests();
-    stepControllerTests();
-    fieldModelTests();
-    formStepModelTests();
-    validationModelTests();
-    apiServiceTests();
-    formServiceTests();
-    jsonParserTests();
-    validatorTests();
-    formBuilderTests();
-    formFieldWidgetTests();
-    stepperWidgetTests();
-  });
+ 
 }
