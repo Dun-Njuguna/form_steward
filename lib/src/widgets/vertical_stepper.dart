f444b3aaaa52727f8b854849c85217d087401b07
Duncan Kariuki <duncankariuki60@gmail.com>
2024-09-21 13:42:04 +0300

    Refactor: Improve layout handling in VerticalStepper widget
    - Replaced  with  inside steps to resolve layout issues.
    - Introduced validation trigger in the step navigation process.
    - Updated step navigation logic to ensure proper transition between steps.
    - Ensured form submission is triggered on the last step.
    - Adjusted  to handle validation before navigating to the next step.

--- lib/src/widgets/vertical_stepper.dart
+++ lib/src/widgets/vertical_stepper.dart
@@ -24,6 +24,7 @@ class VerticalStepper extends StatefulWidget {
 
 class VerticalStepperState extends State<VerticalStepper> {
   late final ValueNotifier<int> _currentStepNotifier;
+  int currentStep = 0;
 
   @override
   void initState() {
@@ -32,15 +33,19 @@ class VerticalStepperState extends State<VerticalStepper> {
   }
 
   void _goToNextStep(int index) {
-    if (index < widget.formSteps.length) {
-      _currentStepNotifier.value = index;
-    }
+  if (index < widget.formSteps.length) {
+    setState(() {
+      currentStep = index;
+    });
+  }
   }
 
   void _goToPreviousStep(int index) {
-    if (index < widget.formSteps.length) {
-      _currentStepNotifier.value = index;
-    }
+  if (index < widget.formSteps.length) {
+    setState(() {
+      currentStep = index;
+    });
+  }
   }
 
   void _submitForm() {
@@ -66,64 +71,53 @@ class VerticalStepperState extends State<VerticalStepper> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ValueListenableBuilder<int>(
-            valueListenable: _currentStepNotifier,
-            builder: (context, currentStep, _) {
-              return Stepper(
-                currentStep: currentStep,
-                onStepTapped: (int index) {
-                  if (currentStep > index) {
-                    _goToPreviousStep(index);
-                  } else {
-                    currentStep == widget.formSteps.length - 1
-                        ? _submitForm()
-                        : _goToNextStep(index);
-                  }
-                },
-                onStepContinue: currentStep == widget.formSteps.length - 1
-                    ? () => _submitForm()
-                    : () => _goToNextStep(currentStep+1),
-                onStepCancel: () => _goToPreviousStep(currentStep-1),
-                steps: _buildSteps(),
-                controlsBuilder: (BuildContext context, ControlsDetails details) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      if (currentStep > 0)
-                        ElevatedButton(
-                          onPressed: details.onStepCancel,
-                          child: const Text('Back'),
-                        ),
-                      const Spacer(),
-                      ElevatedButton(
-                        onPressed: () {
-                          widget.formStewardNotifier.triggerValidation(
-                              widget.formSteps[currentStep].name);
-                          if (widget.formStewardStateNotifier.isStepValid(
-                            stepName: widget.formSteps[currentStep].name,
-                          )) {
-                            details.onStepContinue!();
-                          } else {
-                            widget.formStewardNotifier.reset();
-                          }
-                        },
-                        child: Text(
-                          currentStep == widget.formSteps.length - 1
-                              ? 'Submit'
-                              : 'Next',
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-          ),
-        ),
-      ],
+    return Stepper(
+      currentStep: currentStep,
+      onStepTapped: (int index) {
+        if (currentStep > index) {
+          _goToPreviousStep(index);
+        } else {
+          currentStep == widget.formSteps.length - 1
+              ? _submitForm()
+              : _goToNextStep(index);
+        }
+      },
+      onStepContinue: currentStep == widget.formSteps.length - 1
+          ? () => _submitForm()
+          : () => _goToNextStep(currentStep+1),
+      onStepCancel: () => _goToPreviousStep(currentStep-1),
+      steps: _buildSteps(),
+      controlsBuilder: (BuildContext context, ControlsDetails details) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            if (currentStep > 0)
+              ElevatedButton(
+                onPressed: details.onStepCancel,
+                child: const Text('Back'),
+              ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                widget.formStewardNotifier.triggerValidation(
+                    widget.formSteps[currentStep].name);
+                if (widget.formStewardStateNotifier.isStepValid(
+                  stepName: widget.formSteps[currentStep].name,
+                )) {
+                  details.onStepContinue!();
+                } else {
+                  widget.formStewardNotifier.reset();
+                }
+              },
+              child: Text(
+                currentStep == widget.formSteps.length - 1
+                    ? 'Submit'
+                    : 'Next',
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
